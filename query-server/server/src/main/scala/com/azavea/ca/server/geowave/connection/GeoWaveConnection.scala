85b37ba3fc621923cba2a9f00989051bb8f1f4f3
lossyrob <rdemanuele@gmail.com>
2016-09-12 16:10:18 -0400

    Changed geowave to use geotools when available, added queries

--- query-server/server/src/main/scala/com/azavea/ca/server/geowave/connection/GeoWaveConnection.scala
+++ query-server/server/src/main/scala/com/azavea/ca/server/geowave/connection/GeoWaveConnection.scala
@@ -1,13 +1,16 @@
 package com.azavea.ca.server.geowave.connection
 
 import com.typesafe.config.ConfigFactory
+import mil.nga.giat.geowave.adapter.vector.plugin.{GeoWaveGTDataStore, GeoWavePluginConfig}
 import mil.nga.giat.geowave.datastore.accumulo._
 import mil.nga.giat.geowave.datastore.accumulo.metadata._
 import mil.nga.giat.geowave.datastore.accumulo.operations.config._
+import mil.nga.giat.geowave.core.store.operations.remote.options.DataStorePluginOptions
 import net.ceedubs.ficus.Ficus
 import net.ceedubs.ficus.readers.ArbitraryTypeReader
+import org.geotools.data.{DataStore => GeotoolsDataStore, _}
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 
 object GeoWaveConnection {
@@ -46,4 +49,29 @@ object GeoWaveConnection {
 
   def adapterStore(gwNamespace: String) =
     new AccumuloAdapterStore(basicOperations(gwNamespace))
+
+  def geotoolsDataStore(gwNamespace: String): GeotoolsDataStore = {
+    val dataStoreOptions= new DataStorePluginOptions()
+    val opts = new AccumuloRequiredOptions()
+    opts.setGeowaveNamespace(gwNamespace);
+    opts.setUser(geowaveConfig.user);
+    opts.setPassword(geowaveConfig.password);
+    opts.setInstance(geowaveConfig.instance);
+    opts.setZookeeper(geowaveConfig.zookeepers);
+    dataStoreOptions.selectPlugin(new AccumuloDataStoreFactory().getName())
+    dataStoreOptions.setFactoryOptions(opts)
+
+    val config =
+      dataStoreOptions.getFactoryOptionsAsMap()
+        .asScala
+        .map { case (key, value) => (key, value: java.io.Serializable) }
+        .toMap
+        .asJava
+
+    new GeoWaveGTDataStore(
+      new GeoWavePluginConfig(
+        dataStoreOptions.getFactoryFamily(),
+        config)
+    )
+  }
 }
