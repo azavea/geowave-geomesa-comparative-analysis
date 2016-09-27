d31aebc5d3114a89e2a7b61d77b89b5e397ece71
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-09-27 09:59:29 -0400

    GeoWave CQL Queries

--- query-planning/wave-plan/WavePlan.scala
+++ query-planning/wave-plan/WavePlan.scala
@@ -1,8 +1,8 @@
 package com.azavea.ca.planning
 
 import com.vividsolutions.jts.geom._
-import com.vividsolutions.jts.geom.GeometryFactory
 import mil.nga.giat.geowave.adapter.vector._
+import mil.nga.giat.geowave.adapter.vector.query.cql.CQLQuery
 import mil.nga.giat.geowave.core.geotime.index.dimension._
 import mil.nga.giat.geowave.core.geotime.index.dimension.TemporalBinningStrategy.{ Unit => BinUnit }
 import mil.nga.giat.geowave.core.geotime.ingest._
@@ -14,52 +14,92 @@ import mil.nga.giat.geowave.core.index.sfc.SFCFactory.SFCType
 import mil.nga.giat.geowave.core.index.sfc.tiered.TieredSFCIndexFactory
 import mil.nga.giat.geowave.core.store.DataStore
 import mil.nga.giat.geowave.core.store.index._
-import mil.nga.giat.geowave.core.store.index.PrimaryIndex
 import mil.nga.giat.geowave.core.store.index.writer.IndexWriter
 import mil.nga.giat.geowave.datastore.accumulo._
 import mil.nga.giat.geowave.datastore.accumulo.index.secondary._
 import mil.nga.giat.geowave.datastore.accumulo.metadata._
 import mil.nga.giat.geowave.datastore.accumulo.operations.config.AccumuloOptions
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder
+import org.geotools.referencing.CRS
+import org.opengis.feature.simple.SimpleFeatureType
 
-import scala.math.pow
 
 object WavePlan {
 
+  val rng = new scala.util.Random
+
+  // The current default
+  val MAX_RANGE_DECOMPOSITION = 5000
+
+  // For CQL queries
+  val authorityFactory = CRS.getAuthorityFactory(true)
+  val epsg4326 = authorityFactory.createCoordinateReferenceSystem("EPSG:4326")
+  val sftb = (new SimpleFeatureTypeBuilder).minOccurs(1).maxOccurs(1).nillable(false)
+  val sftName = "CommonSimpleFeatureType"
+  sftb.setName(sftName)
+  sftb.add("when", classOf[java.util.Date])
+  sftb.setCRS(epsg4326)
+  sftb.add("where", classOf[Point])
+  val sft = sftb.buildFeatureType
+  val adapter = new FeatureDataAdapter(sft)
+
+  // For "regular" queries
+  val geometryFactory = new GeometryFactory
+
+  /*******************
+   * CREATE STRATEGY *
+   *******************/
+  def createStrategy(period: String) = {
+    val DIMENSIONS = period match {
+      case "day" | "week" | "year" =>
+        val dim1 = new SFCDimensionDefinition(new LongitudeDefinition, 21)
+        val dim2 = new SFCDimensionDefinition(new LatitudeDefinition(true), 21)
+        val dim3 = period match {
+          case "day" => new SFCDimensionDefinition(new TimeDefinition(BinUnit.DAY), 20)
+          case "week" => new SFCDimensionDefinition(new TimeDefinition(BinUnit.WEEK), 20)
+          case "year" => new SFCDimensionDefinition(new TimeDefinition(BinUnit.YEAR), 20)
+        }
+        Array[SFCDimensionDefinition](dim1, dim2, dim3)
+      case _ =>
+        Array[SFCDimensionDefinition](
+          new SFCDimensionDefinition(new LongitudeDefinition, 31),
+          new SFCDimensionDefinition(new LatitudeDefinition(true), 31)
+        )
+    }
+    TieredSFCIndexFactory.createSingleTierStrategy(DIMENSIONS, SFCType.HILBERT)
+  }
+
+  /********
+   * MAIN *
+   ********/
   def main(args: Array[String]): Unit = {
 
-    val n = 250
-    val rng = new scala.util.Random
+    // Check command line arguments
+    if (args.length < 3) {
+      println(s"arguments: <n> <period> <mode> ...")
+      System.exit(-1)
+    }
 
-    val MAX_RANGE_DECOMPOSITION = 5000
+    // Parse command line arguments
+    val n = args(0).toInt
+    val period = args(1)
+    val cql = args(2) match {
+      case "cql" => true
+      case "loop" => false
+      case s: String => throw new Exception(s"Bad mode: $s")
+    }
 
-    val geometryFactory = new GeometryFactory
-    val DIMENSIONS = Array[SFCDimensionDefinition](
-      new SFCDimensionDefinition(new LongitudeDefinition, 20),
-      new SFCDimensionDefinition(new LatitudeDefinition(true), 20),
-      new SFCDimensionDefinition(new TimeDefinition(BinUnit.YEAR), 20)
-    )
-    // val DIMENSIONS = Array[SFCDimensionDefinition](
-    //   new SFCDimensionDefinition(new LongitudeDefinition, 31),
-    //   new SFCDimensionDefinition(new LatitudeDefinition(true), 31)
-    // )
-    val strategy = TieredSFCIndexFactory.createSingleTierStrategy(DIMENSIONS, SFCType.HILBERT)
+    // Create strategy
+    val strategy = createStrategy(period)
 
-    (-30 to -5).foreach({ bits =>
+    /*******************
+     * PERFORM QUERIES *
+     *******************/
+    if (cql) { // CQL queries
+      val query = CQLQuery.createOptimalQuery(args(3), adapter, null, null)
+      val numericData = query.getIndexConstraints(strategy)
+
       val times = (0 to n).map({ _ =>
-        val x = (rng.nextDouble * 355) - 180.0
-        val y = (rng.nextDouble * 175) - 90.0
-        val envelope = new Envelope(
-          x, x + 180*math.pow(2,bits),
-          y, y + 180*math.pow(2,bits)
-        )
-        val geom = geometryFactory.toGeometry(envelope)
-        val start = new java.util.GregorianCalendar(1970, 0, 2).getTime
-        val end = new java.util.GregorianCalendar(1970, 0, 3).getTime
-        val query = new SpatialTemporalQuery(start, end, geom)
-        // val query = new SpatialQuery(geom)
-
-        val numericData = query.getIndexConstraints(strategy)
-
         val before = System.currentTimeMillis
         val ranges = strategy.getQueryRanges(numericData.get(0), MAX_RANGE_DECOMPOSITION, null).size
         val after = System.currentTimeMillis
@@ -67,7 +107,52 @@ object WavePlan {
         after - before
       }).drop(1)
 
-      println(times.sum / n.toDouble)
-    })
+      println(s"${times.sum / n.toDouble}")
+    }
+    else { // "regular" queries
+      (6 to 31).foreach({ bits =>
+        val fn = { n: String =>
+          n match {
+            case "bits" => math.pow(0.5, bits)
+            case n: String => math.pow(0.5, n.toInt)
+          }
+        }
+
+        val times = (0 to n).map({ _ =>
+          val x1 = (rng.nextDouble * 355) - 180.0
+          val y1 = (rng.nextDouble * 175) - 90.0
+          val x2 = x1 + 360*fn(args(3))
+          val y2 = y1 + 360*fn(args(4))
+
+          val envelope = new Envelope(x1, x2, y1, y2)
+          val geom = geometryFactory.toGeometry(envelope)
+
+          val query = period match {
+            case "day" | "week" | "year" =>
+              val t1 = rng.nextLong % (1000*60*60*24*365)
+              val t2 = t1 + fn(args(5)) * (period match {
+                case "day" => 1000*60*60*24
+                case "week" => 1000*60*60*24*7
+                case "year" => 1000*60*60*24*365
+              })
+              val start = new java.util.Date(t1)
+              val end = new java.util.Date(t2.toLong)
+              new SpatialTemporalQuery(start, end, geom)
+            case _ =>
+              new SpatialQuery(geom)
+          }
+
+          val numericData = query.getIndexConstraints(strategy)
+
+          val before = System.currentTimeMillis
+          val ranges = strategy.getQueryRanges(numericData.get(0), MAX_RANGE_DECOMPOSITION, null).size
+          val after = System.currentTimeMillis
+
+          after - before
+        }).drop(1)
+
+        println(s"$bits, ${times.sum / n.toDouble}")
+      })
+    }
   }
 }
--- query-planning/wave-plan/build.sbt
+++ query-planning/wave-plan/build.sbt
@@ -8,6 +8,11 @@ resolvers ++= Seq(
 
 libraryDependencies ++= Seq(
   "com.vividsolutions" % "jts-core" % Version.jts,
+  "org.geotools" % "gt-coverage" % Version.geotools,
+  "org.geotools" % "gt-epsg-hsql" % Version.geotools,
+  "org.geotools" % "gt-geotiff" % Version.geotools,
+  "org.geotools" % "gt-main" % Version.geotools,
+  "org.geotools" % "gt-referencing" % Version.geotools,
   "mil.nga.giat" % "geowave-adapter-vector" % Version.geowave,
   "mil.nga.giat" % "geowave-core-store" % Version.geowave,
   "mil.nga.giat" % "geowave-datastore-accumulo" % Version.geowave
