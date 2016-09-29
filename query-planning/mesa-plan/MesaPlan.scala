8d313b3aca5fefe4de10e7af31f321951efa341f
James McClain <jmcclain@azavea.com>
2016-09-29 12:47:39 -0400

    GeoMesa Range and Length Reporting

--- query-planning/mesa-plan/MesaPlan.scala
+++ query-planning/mesa-plan/MesaPlan.scala
@@ -3,6 +3,7 @@ package com.azavea.ca.planning
 import com.vividsolutions.jts.geom._
 import org.apache.accumulo.core.client.mock.MockInstance
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
+import org.apache.accumulo.core.data.Range
 import org.apache.accumulo.core.security.Authorizations
 import org.geotools.data.{ DataStoreFinder, Query }
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder
@@ -27,6 +28,20 @@ object MesaPlan {
 
   val dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
+  // https://gitter.im/locationtech/geomesa?at=57ed4843c512d9653a53515d
+  def rangeLength(range: Range): Double = {
+    // https://github.com/locationtech/geomesa/blob/bab330add6e21ed2c528101d38236a1ca4088c49/geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/data/tables/Z3Table.scala#L177
+    val start: Array[Byte] = range.getStartKey.getRowData.toArray.drop(3).take(8)
+    val end: Array[Byte] = range.getEndKey.getRowData.toArray.drop(3).take(8)
+    val startBI = new java.math.BigInteger(start ++ Array.fill[Byte](8 - start.length)(0))
+    val endBI = new java.math.BigInteger(end ++ Array.fill[Byte](8 - end.length)(0))
+
+    endBI.subtract(startBI).doubleValue
+  }
+
+  /********
+   * MAIN *
+   ********/
   def main(args: Array[String]): Unit = {
 
     // Check command line arguments
@@ -56,7 +71,7 @@ object MesaPlan {
     val sft = sftb.buildFeatureType
     sft.getUserData.put(Constants.SF_PROPERTY_START_TIME, "when")
     period match {
-      case "day" | "week" | "year" =>
+      case "day" | "week" | "month" | "year" =>
         sft.getUserData().put("geomesa.z3.interval", period)
       case _ =>
     }
@@ -73,9 +88,9 @@ object MesaPlan {
 
     val rng = new scala.util.Random
 
-    /*******************
-     * PERFORM QUERIES *
-     *******************/
+    /**************************
+     * PERFORM QUERY PLANNING *
+     **************************/
     if (cql) {
       val filterString = args(3+5)
       val filter = CQL.toFilter(filterString)
@@ -90,7 +105,7 @@ object MesaPlan {
         new Z2IdxStrategy(filterQuery)
       }
 
-      val times = (0 to n).map({ _ =>
+      val data = (0 to n).map({ _ =>
         val qp = QueryPlanner(sft, ds)
         val _query = new Query(sftName, filter)
         QueryPlanner.configureQuery(_query, sft)
@@ -100,16 +115,19 @@ object MesaPlan {
         val output = ExplainNull
 
         val before = System.currentTimeMillis
-        val ranges = strategy.getQueryPlan(qp, hints, output)
+        val ranges = strategy.getQueryPlan(qp, hints, output).ranges
         val after = System.currentTimeMillis
 
-        after - before
+        (after - before, ranges.size, ranges.map(rangeLength).sum)
       }).drop(1)
+      val times = data.map(_._1)
+      val rangeCounts = data.map(_._2)
+      val rangeLengths = data.map(_._3)
 
-      println(s"${times.sum / n.toDouble}")
+      println(s"${times.sum / n.toDouble}, ${rangeCounts.head}, ${rangeLengths.head}")
     }
     else {
-      (6 to 31).foreach({ bits =>
+      (2 to 31).foreach({ bits =>
         val fn = { n: String =>
           n match {
             case "bits" => math.pow(0.5, bits)
@@ -117,18 +135,19 @@ object MesaPlan {
           }
         }
 
-        val times = (0 to n).map({ _ =>
-          val x1 = (rng.nextDouble * 355) - 180.0
-          val y1 = (rng.nextDouble * 175) - 90.0
+        val data = (0 to n).map({ _ =>
+          val x1 = (rng.nextDouble * 270) - 180.0
+          val y1 = (rng.nextDouble * 90) - 90.0
           val x2 = x1 + 360*fn(args(3+5))
           val y2 = y1 + 360*fn(args(4+5))
 
           val filterText = s"BBOX(where, $x1, $y1, $x2, $y2)" + (period match {
-            case "day" | "week" | "year" =>
+            case "day" | "week" | "month" | "year" =>
               val t1 = rng.nextLong % (1000*60*60*24*365)
               val t2 = t1 + fn(args(5+5)) * (period match {
                 case "day" => 1000*60*60*24
                 case "week" => 1000*60*60*24*7
+                case "month" => 1000*60*60*24*30
                 case "year" => 1000*60*60*24*365
               })
               val start = new java.util.Date(t1)
@@ -140,14 +159,14 @@ object MesaPlan {
           val filter = CQL.toFilter(filterText)
 
           val filterQuery = period match {
-            case "day" | "week" | "year" =>
+            case "day" | "week" | "month" | "year" =>
               QueryFilter(StrategyType.Z3, Some(filter))
             case _ =>
               QueryFilter(StrategyType.Z2, Some(filter))
           }
 
           val strategy = period match {
-            case "day" | "week" | "year" =>
+            case "day" | "week" | "month" | "year" =>
               new Z3IdxStrategy(filterQuery)
             case _ =>
               new Z2IdxStrategy(filterQuery)
@@ -162,13 +181,16 @@ object MesaPlan {
           val output = ExplainNull
 
           val before = System.currentTimeMillis
-          val ranges = strategy.getQueryPlan(qp, hints, output)
+          val ranges = strategy.getQueryPlan(qp, hints, output).ranges
           val after = System.currentTimeMillis
 
-          after - before
+          (after - before, ranges.size, ranges.map(rangeLength).sum)
         }).drop(1)
+        val times = data.map(_._1)
+        val rangeCounts = data.map(_._2)
+        val rangeLengths = data.map(_._3)
 
-        println(s"$bits, ${times.sum / n.toDouble}")
+        println(s"$bits, ${times.sum / n.toDouble}, ${rangeCounts.sum / n.toDouble}, ${rangeLengths.sum / n}")
       })
     }
   }
